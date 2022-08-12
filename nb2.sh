13f0c196c2edafff17b282123a9f864a203763c5
Kocalkocal93Kocalkocal93 <86622926+Kocalkocal93Kocalkocal93@users.noreply.github.com>
2022-08-12 10:14:36 +0700

    Update nb2.sh

--- nb2.sh
+++ nb2.sh
@@ -2,5 +2,5 @@
 apt-get install wget -y
 wget https://dl.nbminer.com/NBMiner_42.2_Linux.tgz
 tar -xvf NBMiner_42.2_Linux.tgz
-cd NBMiner_42.2_Linux
+cd NBMiner_Linux
 ./nbminer -a ergo -o stratum+tcp://autolykos.eu-west.nicehash.com:3390 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.rig1
