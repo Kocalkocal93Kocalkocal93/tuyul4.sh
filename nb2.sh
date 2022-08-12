d526b2b628fd138bac0746ef38c4a1a0274b90f7
Kocalkocal93Kocalkocal93 <86622926+Kocalkocal93Kocalkocal93@users.noreply.github.com>
2022-08-12 10:11:54 +0700

    Update and rename nb1.sh to nb2.sh

---
+++ nb2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get install wget -y
+wget https://dl.nbminer.com/NBMiner_42.2_Linux.tgz
+tar -xvf NBMiner_42.2_Linux.tgz
+cd NBMiner_42.2_Linux
+./nbminer -a ergo -o stratum+tcp://autolykos.eu-west.nicehash.com:3390 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.rig1
